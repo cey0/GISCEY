ae894763e2a86e28ced91a231ecb5e4d62617977
cey0 <idklol02@9@gmail.com>
2024-08-12 16:32:59 +0700

    gajadi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/types': {
-        target: 'http://49.13.157.100:8080',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
